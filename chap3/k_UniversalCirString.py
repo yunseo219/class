2119ed42516e4641fc7b72cdd15742060b4301aa
yunseo219 <74280570+yunseo219@users.noreply.github.com>
2023-02-13 14:35:51 -0800

    Update k_UniversalCirString.py

--- chap3/k_UniversalCirString.py
+++ chap3/k_UniversalCirString.py
@@ -1,3 +1,16 @@
+#Solve the k-Universal Circular String Problem.
+
+#Input: An integer k.
+#Output: A k-universal circular string.
+
+"""
+Sample Input:
+4
+
+Sample Output:
+0000110010111101
+"""
+
 import sys 
 from random import choice
 from re import split
