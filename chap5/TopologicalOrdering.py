2f4f2266780b9721e94e2fe665366dff8dda77d3
yunseo219 <74280570+yunseo219@users.noreply.github.com>
2023-02-13 18:18:24 -0800

    Create TopologicalOrdering.py

---
+++ chap5/TopologicalOrdering.py
@@ -0,0 +1,15 @@
+"""
+Code Challenge: Implement TopologicalOrdering.
+
+Input: The adjacency list of a graph (with nodes represented by integers).
+Output: A topological ordering of this graph.
+
+Sample Input:
+0 -> 1
+1 -> 2
+3 -> 1
+4 -> 2
+
+Sample Output:
+0, 3, 4, 1, 2
+"""
