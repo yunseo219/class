afc3e319bcb83b2ec5248a49992d2ba190983159
yunseo219 <74280570+yunseo219@users.noreply.github.com>
2023-02-13 17:43:16 -0800

    Update ManhattanTourist.py

--- chap5/ManhattanTourist.py
+++ chap5/ManhattanTourist.py
@@ -1,3 +1,25 @@
+"""
+Input: Integers n and m, followed by an n × (m + 1) matrix Down and an (n + 1) × m matrix Right. The two matrices are separated by the "-" symbol.
+Output: The length of a longest path from source (0, 0) to sink (n, m) in the rectangular grid whose edges are defined by the matrices Down and Right.
+
+Sample Input:
+4 4
+1 0 2 4 3
+4 6 5 2 1
+4 4 5 2 1
+5 6 8 5 3
+-
+3 2 4 0
+3 2 4 2
+0 7 3 3
+3 3 0 2
+1 3 2 2
+
+Sample Output:
+34
+"""
+
+
 def ManhattanTourist(n, m, Down, Right):
     S = [[0 for tom in range(m + 1)] for ton in range(n + 1)]
     for i in range(1, n + 1):
