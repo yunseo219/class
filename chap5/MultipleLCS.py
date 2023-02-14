ba0a714099da0513a5cf93178505e5d240eadfd5
yunseo219 <74280570+yunseo219@users.noreply.github.com>
2023-02-14 00:13:12 -0800

    Update and rename MLCsubsequence.py to MultipleLCS.py

--- chap5/MultipleLCS.py
+++ chap5/MultipleLCS.py
@@ -1,3 +1,20 @@
+"""
+Solve the Multiple Longest Common Subsequence Problem.
+Input: Three DNA strings of length at most 10.
+Output: The length of a longest common subsequence of these three strings, followed by a multiple alignment of the three strings corresponding to such an alignment.
+Extra Dataset and Test Datasets
+
+Sample Input:
+ATATCCG
+TCCGA
+ATGTACTG
+
+Sample Output:
+3
+ATATCC-G-
+---TCC-GA
+ATGTACTG-
+"""
 
 v = 'AACAGAAG'
 w = 'GTTGAATG'
