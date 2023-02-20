8c333eb3f15b2a5462538805e9213f650c6d0d48
yunseo219 <74280570+yunseo219@users.noreply.github.com>
2023-02-20 11:59:59 -0800

    Update BWT.py

--- chap9/BWT.py
+++ chap9/BWT.py
@@ -1,4 +1,17 @@
+"""
+Burrows-Wheeler Transform Construction Problem: Construct the Burrows-Wheeler transform of a string.
 
+Input: A string Text.
+Output: BWT(Text).
+
+Note: Although it is possible to construct the Burrows-Wheeler transform in O(|Text|) time and space, we do not expect you to implement such a fast algorithm. In other words, it is perfectly fine to produce BWT(Text) by first producing the complete Burrows-Wheeler matrix M(Text).
+
+Sample Input:
+GCGTGCCTGGTCA$
+
+Sample Output:
+ACTGGCT$TGCGGC
+"""
 #cyclic rotation is defined by chopping off a suffix from the end of Text and appending 
 #this suffix to the beginning of Text. Next — similarly to suffix arrays 
 #— order all the cyclic rotations of Text lexicographically to form a |Text| × |Text| matrix of symbols
