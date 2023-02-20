cb9bf878d89be4e3e9c49e8172eaa6376bb1952f
yunseo219 <74280570+yunseo219@users.noreply.github.com>
2023-02-20 01:55:53 -0800

    Create SuffixTreeConstruction.py

---
+++ chap9/SuffixTreeConstruction.py
@@ -0,0 +1,23 @@
+"""
+Code Challenge: Solve the Suffix Tree Construction Problem.
+
+Input: A string Text.
+Output: The edge labels of SuffixTree(Text). You may return these strings in any order.
+
+Sample Input:
+ATAAATG$
+
+Sample Output:
+AAATG$
+G$
+T
+ATG$
+TG$
+A
+A
+AAATG$
+G$
+T
+G$
+$
+"""
