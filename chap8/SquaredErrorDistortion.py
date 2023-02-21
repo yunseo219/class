33002302aebfd095e624313a769494f366ff382a
yunseo219 <74280570+yunseo219@users.noreply.github.com>
2023-02-20 16:23:08 -0800

    Update SquaredErrorDistortion.py

--- chap8/SquaredErrorDistortion.py
+++ chap8/SquaredErrorDistortion.py
@@ -1,3 +1,29 @@
+"""
+Code Challenge: Solve the Squared Error Distortion Problem.
+
+Input: Integers k and m, followed by a set of centers Centers and a set of points Data.
+Output: The squared error distortion Distortion(Data, Centers).
+
+Sample Input:
+2 2
+2.31 4.55
+5.96 9.08
+--------
+3.42 6.03
+6.23 8.25
+4.76 1.64
+4.47 4.33
+3.95 7.61
+8.93 2.97
+9.74 4.03
+1.73 1.28
+9.72 5.01
+7.27 3.77
+
+Sample Output:
+18.246
+"""
+
 import math
 
 def Euclidean_distance(v, w):
